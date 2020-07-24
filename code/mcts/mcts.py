9fa2b3844fc11a17c6f56d1c889c146eca987305
ben <benjamin.p.riviere@gmail.com>
2020-07-23 18:23:04 -0700

    mcts slightly better

--- code/mcts/mcts.py
+++ code/mcts/mcts.py
@@ -1,27 +1,35 @@
 
 import numpy as np 
 from collections import defaultdict
+import itertools
 
 import sys
 sys.path.append("../")
 from utilities import dbgp
 
-# https://int8.io/monte-carlo-tree-search-beginners-guide/
+# https://int8.io/monte-carlo-tree-search-beginners-guide/ 
 # https://github.com/int8/monte-carlo-tree-search/blob/master/mctspy/tree/nodes.py 
-# https://github.com/int8/monte-carlo-tree-search/blob/master/mctspy/tree/search.py
+# https://github.com/int8/monte-carlo-tree-search/blob/master/mctspy/tree/search.py 
+
+# team 1 trying to maximize value 
+# team 2 trying to minimize value 
 
 # parameters
 r_capture = 0.05 
-goal = np.array([0.6,0]) 
+goal = np.array([0.45,0.05]) 
 u_max_1 = 0.1 
 u_max_2 = 0.2 
 v_max_1 = 0.1 
 v_max_2 = 0.2 
 dt 		= 0.25 
-num_simulations = 500 
+num_simulations = 100
 rollout_horizon = 10 
+# team_1_idxs = [0] 
+# team_2_idxs = [1] 
+# team_1_idxs = [0,1] 
+# team_2_idxs = [2] 
 team_1_idxs = [0] 
-team_2_idxs = [1] 
+team_2_idxs = [1,2] 
 c_param = 1.4 
 
 class Node: 
@@ -31,18 +39,20 @@ class Node:
 		self.team_1_turn = team_1_turn 
 		self.parent = parent 
 		self.action_to_node = action_to_node 
-		self.done, self.score = evaluate_game(self.state)
-		self.untried_actions = get_legal_actions(self)
+		self.done, self.reward_1, self.reward_2 = evaluate_game(self.state) 
+		self.untried_actions = get_legal_actions(self.state,self.team_1_turn) 
 		self.number_of_visits = 0.
-		self.value = 0.0 
+		self.value_1 = 0.
+		self.value_2 = 0.
 		self.children = [] 
 
 	@property
 	def q(self):
 		if self.parent.team_1_turn:
-			return -1*self.value
+			value = self.value_1
 		else:
-			return self.value 
+			value = self.value_2 
+		return value
 
 	@property
 	def n(self):
@@ -56,8 +66,8 @@ class Node:
 
 	def expand(self):
 		action = self.untried_actions.pop()
-		next_state = forward(self.state,action)
-		next_turn = self.team_1_turn == False
+		next_state = forward(self.state,action,self.team_1_turn)
+		next_turn = not self.team_1_turn 
 		child_node = Node(next_state,next_turn,parent=self,action_to_node=action)
 		self.children.append(child_node)
 		return child_node
@@ -67,6 +77,8 @@ class Node:
 		for c in self.children: 
 			weight = (c.q / c.n) + c_param * np.sqrt((2 * np.log(self.n) / c.n))
 			weights.append(weight)
+			# dbgp('weight',weight)
+			# dbgp('c.action_to_node',c.action_to_node)
 		return self.children[np.argmax(weights)]
 
 
@@ -76,31 +88,56 @@ def sample(actions):
 
 def evaluate_game(state):
 
+	# print('state',state)
+
 	dist_robots = np.zeros((state.shape[0],state.shape[0]))
 	for idx_i,state_i in enumerate(state):
 		for idx_j,state_j in enumerate(state):
 			dist_robots[idx_i,idx_j] = np.linalg.norm(state_i[0:2]-state_j[0:2])
 	
-	dist_goal = np.zeros(len(team_1_idxs))
-	for idx_i in team_1_idxs:
-		dist_goal[idx_i] = np.linalg.norm(state[idx_i,0:2] - goal)
+	# print('dist_robots',dist_robots)
 
-	score = 0 
+	reward_1,reward_2 = 0, 0
 	done = np.zeros(len(team_1_idxs),dtype=bool)
 	for idx_i in team_1_idxs: 
-		if dist_goal[idx_i] < r_capture:
+		dist_goal = np.linalg.norm(state[idx_i,0:2] - goal)
+		if dist_goal < r_capture:
 			done[idx_i] = True
+			reward_1 += 1 
+			reward_2 += 0
+
 		elif np.any(dist_robots[idx_i,team_2_idxs] < r_capture):
 			done[idx_i] = True
-		score += dist_goal[idx_i] 
+			reward_1 += 0 
+			reward_2 += 1
 
-	done = np.all(done)
-	return done, score
+		else:
+			# composite_dist = dist_goal + 10/np.min(dist_robots[idx_i,team_2_idxs])
+			# reward_1 += np.exp(-1*composite_dist)
+			# reward_2 += -reward_1
+
+			reward_1 += np.exp(-1*dist_goal)
+			# reward_2 += np.exp(-1*np.min(dist_robots[idx_i,team_2_idxs]))
+			reward_2 += np.exp(-1*np.sum(dist_robots[idx_i,team_2_idxs]))
+
+	reward_1 /= len(team_1_idxs)
+	reward_2 /= len(team_1_idxs)
+
+	# print('reward_1',reward_1)
+	# print('reward_2',reward_2)
+		
+	done = np.any(done)
+	return done,reward_1,reward_2
 
 
-def forward(state,action):
-	next_state = np.zeros((state.shape))
-	for idx_i in range(state.shape[0]):
+def forward(state,action,team_1_turn):
+	if team_1_turn: 
+		idxs = team_1_idxs
+	else:
+		idxs = team_2_idxs
+
+	next_state = np.copy(state)
+	for idx_i in idxs:
 		next_state[idx_i,:] = forward_per_robot(state[idx_i,:],action[idx_i,:])
 	return next_state
 
@@ -122,47 +159,60 @@ def forward_per_robot(state_i,action_i):
 	return next_state_i
 
 
-def get_legal_actions(node):
-	if node.team_1_turn:
-		idx = 0 
+def get_legal_actions(state,team_1_turn):
+	if team_1_turn:
+		idxs = team_1_idxs
 		u_max = u_max_1 
 		v_max = v_max_1 
 	else: 
-		idx = 1 
+		idxs = team_2_idxs
 		u_max = u_max_2 
 		v_max = v_max_2 
 
-	actions = []
 	u_xs = u_max*np.asarray([-1,0,1])
 	u_ys = u_max*np.asarray([-1,0,1])
-	for u_x in u_xs:
-		for u_y in u_ys:
-			action = np.zeros((node.state.shape[0],2))
-			action[idx,:] = np.array((u_x,u_y))
-			# velocity check 
-			if np.linalg.norm(forward_per_robot(node.state[idx,:],action[idx,:])[2:]) < v_max:
-				actions.append(action)
 
+	master_lst = [] 
+	for idx in idxs:
+		master_lst.extend([u_xs,u_ys])
+	master_lst = list(itertools.product(*master_lst))
+
+	actions = []
+	for elem in list(master_lst):
+		action = np.zeros((state.shape[0],2))
+		for action_idx,robot_idx in enumerate(idxs): 
+			action[robot_idx,:] = np.array(elem)[action_idx*2 + np.arange(2)] 
+		actions.append(action)
 	return actions
 
 
-def backpropagate(node,reward):
+def backpropagate(node,reward_1,reward_2):
 	node.number_of_visits += 1.
-	node.value += reward
+	node.value_1 += reward_1
+	node.value_2 += reward_2
 	if node.parent:
-		backpropagate(node.parent,reward)
+		backpropagate(node.parent,reward_1,reward_2)
 
 
 def rollout(node):
+	value_1,value_2 = 0,0
+	state = node.state
+	team_1_turn = node.team_1_turn
 	for i in range(rollout_horizon):
-		actions = get_legal_actions(node)
+		actions = get_legal_actions(state,team_1_turn)
 		action = sample(actions)
-		state = forward(node.state,action)
-		done,score = evaluate_game(node.state)
+		state = forward(state,action,team_1_turn)
+		done,reward_1,reward_2 = evaluate_game(state)
+
+		value_1 = reward_1
+		value_2 = reward_2
+		team_1_turn = not team_1_turn 
+		
 		if done:
 			print('breaking at i/rollout_horizon = {}/{}'.format(i,rollout_horizon))
 			break 
-	return score
+
+	return value_1,value_2
 
 
 def tree_policy(root_node):
@@ -179,14 +229,17 @@ def best_action(root_state,team_1_turn):
 	root_node = Node(root_state,team_1_turn)
 	
 	if root_node.is_terminal_node():
-		return root_node.state,np.zeros((root_state.shape[0],2))
+		return None,None
 
 	for _ in range(num_simulations):
 		current_node = tree_policy(root_node)
-		reward = rollout(current_node)
-		backpropagate(current_node,reward)
+		reward_1,reward_2 = rollout(current_node)
+		backpropagate(current_node,reward_1,reward_2)
 
+	# print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+	# print('text')
 	best_child = root_node.best_child(c_param=0.0)
+	# exit()
 
 	return best_child.state, best_child.action_to_node
 
--- code/mcts/run.py
+++ code/mcts/run.py
@@ -6,39 +6,45 @@ import matplotlib.pyplot as plt
 
 initial_state = np.array([
 	[0,0,0,0],
+	# [0,0.25,0,0],
+	[0,1,0,0],
 	[1,0,0,0],
 	])
 n_timesteps = 40 
-team_1_idxs = [0]
-team_2_idxs = [1]
-dt = 0.25
+goal = mcts.goal
 
 timesteps = [] 
 states = [initial_state]
 for timestep in range(n_timesteps):
+
 	print(timestep/n_timesteps)
-	_,team_1_action = mcts.best_action(states[timestep],True)
-	_,team_2_action = mcts.best_action(states[timestep],False)
-
 	old_state = states[timestep]
-	new_state = np.zeros((old_state.shape))
-	for idx in range(old_state.shape[0]):
-		if idx in team_1_idxs:
-			new_state[idx,:] = mcts.forward_per_robot(old_state[idx,:],team_1_action[idx,:])
-		else:
-			new_state[idx,:] = mcts.forward_per_robot(old_state[idx,:],team_2_action[idx,:])
+	
+	_,team_1_action = mcts.best_action(old_state,True)
+	_,team_2_action = mcts.best_action(old_state,False)
 
-	timesteps.append(timestep)
-	states.append(new_state)
+	if team_1_action is not None and team_2_action is not None:
+		new_state = np.zeros((old_state.shape))
+		for idx in range(old_state.shape[0]):
+			if idx in mcts.team_1_idxs:
+				new_state[idx,:] = mcts.forward_per_robot(old_state[idx,:],team_1_action[idx,:])
+			else:
+				new_state[idx,:] = mcts.forward_per_robot(old_state[idx,:],team_2_action[idx,:])
+		timesteps.append(timestep)
+		states.append(new_state)
 
-states = np.asarray(states) # print()
+	else:
+		break 
+
+states = np.asarray(states) 
+timesteps = np.asarray(timesteps)
+
 fig,ax = plt.subplots()
-line = ax.plot(states[:,0,0],states[:,0,1],label='robot 0',linewidth=3)
-ax.scatter(states[-1,0,0],states[-1,0,1],marker='o',color=line[0].get_color())
-line = ax.plot(states[:,1,0],states[:,1,1],label='robot 1',linewidth=3)
-ax.scatter(states[-1,1,0],states[-1,1,1],marker='o',color=line[0].get_color())
-ax.scatter(0.6,0,label='goal',color='green',marker='o')
 ax.grid(True)
+ax.scatter(mcts.goal[0],mcts.goal[1],label='goal',color='green',marker='o')
+for i in range(initial_state.shape[0]):
+	line = ax.plot(states[:,i,0],states[:,i,1],label='robot {}'.format(i),linewidth=3)
+	ax.scatter(states[:,i,0],states[:,i,1],marker='o',color=line[0].get_color())
 ax.legend()
+# ax.set_aspect('equal')
 plt.show()
-
