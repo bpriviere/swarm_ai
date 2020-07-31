fixed depth option, velocity scaling, minor plot fix, change reward fnc, changed boundary edge case reward accrue

code/mcts/mcts.py
@@ -44,14 +44,16 @@ class RobotDynamics:
 			self.idxs = param.team_2_idxs
 
 	def step(self, state, action):
-		return np.dot(self.A, state) + np.dot(self.B, action)
+		next_state = np.dot(self.A, state) + np.dot(self.B, action)
+		alpha = np.linalg.norm(next_state[2:]) / self.v_max
+		next_state[2:] = next_state[2:] / np.max((alpha,1))
+		return next_state
 
 	def valid(self, state):
 		if not (state[0] > self.param.env_xlim[0] and \
 				state[0] < self.param.env_xlim[1] and \
 				state[1] > self.param.env_ylim[0] and \
-				state[1] < self.param.env_ylim[1] and \
-				np.linalg.norm(state[2:]) <= self.v_max):
+				state[1] < self.param.env_ylim[1]):
 			return False
 		return True
 
@@ -95,10 +97,11 @@ class State:
 		self.dist_robots = np.linalg.norm(self.state[:,0:2][:, np.newaxis] - self.state[:,0:2],axis=2)
 		self.dist_goal = np.linalg.norm(self.state[self.param.team_1_idxs,0:2] - self.param.goal, axis=1)
 		self.done = self.__compute_done(prev_done) 
+		self.not_done = list(set(self.param.team_1_idxs)-set(self.done))
 		if turn:
 			self.dynamics = self.dynamicsA
 			# the indices are the ones of team 1 that are still actively playing
-			self.idxs = list(set(self.param.team_1_idxs)-set(self.done))
+			self.idxs = self.not_done
 		else:
 			self.dynamics = self.dynamicsB
 			self.idxs = self.param.team_2_idxs
@@ -153,11 +156,7 @@ class State:
 
 	def eval_predict(self):
 		reward_1,reward_2 = 0,0
-		for idx in self.param.team_1_idxs: 
-			reward_1 += np.exp(-1*self.dist_goal[idx])
-			# reward_2 += np.exp(-1*np.sum(self.dist_robots[idx,self.param.team_2_idxs]))
-		reward_1 /= len(self.param.team_1_idxs)
-		# reward_2 /= len(self.param.team_1_idxs)
+		reward_1 = np.exp(-1*np.min(self.dist_goal[self.not_done]))
 		reward_2 = 1 - reward_1
 		return reward_1,reward_2
 
@@ -297,25 +296,75 @@ class Tree:
 		value_1,value_2,eta,depth = node.get_value_to_come()
 		reward_1,reward_2 = state.eval_reward()
 		value_1,value_2,eta = eval_value(value_1,value_2,eta,reward_1,reward_2,self.param.gamma,depth)
-		for i in range(self.param.rollout_horizon):
-			untried_actions = state.possible_actions().copy()
-			random.shuffle(untried_actions)
-			if state.is_terminal() or len(untried_actions) == 0: 
-				for k in range(self.param.rollout_horizon-i):
-					value_1,value_2,eta = eval_value(value_1,value_2,eta,reward_1,reward_2,self.param.gamma,i+k+depth)
-				return value_1/eta,value_2/eta
-			while len(untried_actions) > 0:
-				action = untried_actions.pop()
-				next_state = state.forward(action)
-				if next_state is not None:
-					state = next_state
-					break
-				if len(untried_actions) == 0:
-					for k in range(self.param.rollout_horizon-i):
-						value_1,value_2,eta = eval_value(value_1,value_2,eta,reward_1,reward_2,self.param.gamma,i+k+depth)
-					# return reward_1,reward_2
+
+		if self.param.fixed_tree_depth_on:
+
+			i = 0 
+			while i + depth < self.param.fixed_tree_depth:
+				untried_actions = state.possible_actions().copy()
+				random.shuffle(untried_actions)
+				
+				# terminal state 
+				if state.is_terminal() or len(untried_actions) == 0: 
+					k = 0 
+					while k + i + depth < self.param.fixed_tree_depth:
+						value_1,value_2,eta = eval_value(value_1,value_2,eta,reward_1,reward_2,\
+							self.param.gamma,i+k+depth)
+						k += 1 
 					return value_1/eta,value_2/eta
-			reward_1,reward_2 = state.eval_reward()
-			value_1,value_2,eta = eval_value(value_1,value_2,eta,reward_1,reward_2,self.param.gamma,i+depth)
-		# return reward_1,reward_2
-		return value_1/eta,value_2/eta
+				
+				# get next state
+				while len(untried_actions) > 0:
+					action = untried_actions.pop()
+					next_state = state.forward(action)
+					if next_state is not None:
+						state = next_state
+						break
+
+					# no available next state, terminal 
+					if len(untried_actions) == 0:
+						k = 0 
+						while k + i + depth < self.param.fixed_tree_depth:
+							value_1,value_2,eta = eval_value(value_1,value_2,eta,0,0,\
+								self.param.gamma,i+k+depth)
+							k += 1 
+						return value_1/eta,value_2/eta
+
+				# nominal rollout
+				reward_1,reward_2 = state.eval_reward()
+				value_1,value_2,eta = eval_value(value_1,value_2,eta,reward_1,reward_2,\
+					self.param.gamma,i+depth)
+				i += 1 
+			return value_1/eta,value_2/eta
+
+		else: 
+
+			for i in range(self.param.rollout_horizon):
+				untried_actions = state.possible_actions().copy()
+				random.shuffle(untried_actions)
+				if state.is_terminal() or len(untried_actions) == 0: 
+					for k in range(self.param.rollout_horizon-i):
+						value_1,value_2,eta = eval_value(value_1,value_2,eta,reward_1,reward_2,\
+							self.param.gamma,i+k+depth)
+						if k + i + depth >= self.param.effective_depth: 
+							break 
+					return value_1/eta,value_2/eta
+				while len(untried_actions) > 0:
+					action = untried_actions.pop()
+					next_state = state.forward(action)
+					if next_state is not None:
+						state = next_state
+						break
+					if len(untried_actions) == 0:
+						for k in range(self.param.rollout_horizon-i):
+							value_1,value_2,eta = eval_value(value_1,value_2,eta,0,0,\
+								self.param.gamma,i+k+depth)
+							if k + i + depth >= self.param.effective_depth: 
+								break 
+						return value_1/eta,value_2/eta
+				reward_1,reward_2 = state.eval_reward()
+				value_1,value_2,eta = eval_value(value_1,value_2,eta,reward_1,reward_2,\
+					self.param.gamma,i+depth)
+				if i + depth >= self.param.fixed_tree_depth: 
+					break 
+			return value_1/eta,value_2/eta

code/param.py
@@ -27,6 +27,7 @@ class Param:
 		self.r_comm = 1.6
 		
 		# environment
+		self.goal_line_x = 0.6
 		large_env = False 
 		if large_env:
 			self.env_xlim = [0,1]
@@ -35,7 +36,6 @@ class Param:
 			self.reset_ylim_A = [0,1]
 			self.reset_xlim_B = [0.8,1]
 			self.reset_ylim_B = [0,1]
-			self.goal_line_x = 0.6
 			self.goal = np.array([0.5,0.5])
 		else: 
 			l = 0.5
@@ -45,17 +45,18 @@ class Param:
 			self.reset_ylim_A = [0.5*l,0.5*l]
 			self.reset_xlim_B = [0.9*l,0.9*l]
 			self.reset_ylim_B = [0.5*l,0.5*l]
-			self.goal_line_x = 0.6
-			self.goal = np.array([0.5*l,0.75*l])
+			self.goal = np.array([0.9*l,0.75*l])
 
 		# mcts parameters 
-		self.tree_size = 100
-		self.rollout_horizon = 100
+		self.tree_size = 1000
+		self.fixed_tree_depth = 100
+		self.fixed_tree_depth_on = True
+		self.rollout_horizon = 1000
 		self.c_param = 1.4
 		self.gamma = 1.0
 		
 		# nodes 
-		self.num_nodes_A = 1
+		self.num_nodes_A = 2
 		self.num_nodes_B = 1
 		
 		# estimator parameters
@@ -70,12 +71,12 @@ class Param:
 		# policy 
 		self.rhc_horizon = 5
 		self.lambda_u = 0.01
-		self.speed_limit_a = 0.25 # 0.05 
+		self.speed_limit_a = 0.5 # 0.05 
 		self.speed_limit_b = 0.5 # 0.1 
-		self.acceleration_limit_a = 0.10
-		self.acceleration_limit_b = 0.20
+		self.acceleration_limit_a = 0.5
+		self.acceleration_limit_b = 0.5 
 		self.danger_radius = 0.1
-		self.tag_radius = 0.075
+		self.tag_radius = 0.05
 
 		# path stuff
 		self.current_results_dir = '../current_results'

code/plotter.py
@@ -278,6 +278,7 @@ def plot_tree_results(sim_result):
 	ax = axs[0,0]
 	ax.grid(True)
 	ax.set_aspect('equal')
+	ax.set_title('State Space')
 	ax.add_patch(mpatches.Circle(goal, tag_radius, color=goal_color,alpha=0.5))
 	ax.plot(np.nan,np.nan,color=team_1_color,label='attackers')
 	ax.plot(np.nan,np.nan,color=team_2_color,label='defenders')
@@ -294,37 +295,40 @@ def plot_tree_results(sim_result):
 	# value func 
 	ax = axs[0,1] 
 	ax.grid(True)
-	ratio = (times[-1] - times[0])/(1 - 0) 
-	ax.set_aspect(ratio)
+	# ratio = (times[-1] - times[0])/(1 - 0) 
+	# ax.set_aspect(ratio)
+	ax.set_title('Value Function')
 	ax.plot(times,values[:,0],color=team_1_color)
 	ax.plot(times,values[:,1],color=team_2_color)
 
 	# time varying velocity
 	ax = axs[1,0]
 	ax.grid(True)
-	ratio = (times[-1] - times[0])/(max((speed_limit_a,speed_limit_b))-0)
-	ax.set_aspect(ratio)
-	ax.axhline(speed_limit_a,color=team_1_color,linestyle='--',label=r'$\overline{v}_1$')
-	ax.axhline(speed_limit_b,color=team_2_color,linestyle='--',label=r'$\overline{v}_2$')
+	# ratio = (times[-1] - times[0])/(max((speed_limit_a,speed_limit_b))-0)
+	# ax.set_aspect(ratio)
+	ax.axhline(speed_limit_a,color=team_1_color,linestyle='--')
+	ax.axhline(speed_limit_b,color=team_2_color,linestyle='--')
+	ax.set_title('Speed Profile')
 	for i in range(num_nodes):
 		color = team_2_color 
 		if i in team_1_idxs:
 			color = team_1_color
 		ax.plot(times,np.linalg.norm(states[:,i,2:],axis=1),color=color)
-	ax.legend(loc='upper right')
+	# ax.legend(loc='upper right')
 
 	# time varying acc
 	ax = axs[1,1]
 	ax.grid(True)
-	ratio = (times[-1] - times[0])/(max((acceleration_limit_a,acceleration_limit_b))-0)
-	ax.set_aspect(ratio)
-	ax.axhline(acceleration_limit_a,color=team_1_color,linestyle='--',label=r'$\overline{u}_1$')
-	ax.axhline(acceleration_limit_b,color=team_2_color,linestyle='--',label=r'$\overline{u}_2$')
+	# ratio = (times[-1] - times[0])/(max((acceleration_limit_a,acceleration_limit_b))-0)
+	# ax.set_aspect(ratio)
+	ax.axhline(acceleration_limit_a,color=team_1_color,linestyle='--')
+	ax.axhline(acceleration_limit_b,color=team_2_color,linestyle='--')
+	ax.set_title('Acceleration Profile')
 	for i in range(num_nodes):
 		color = team_2_color 
 		if i in team_1_idxs:
 			color = team_1_color
 		ax.plot(times,np.linalg.norm(actions[:,i],axis=1),color=color)
-	ax.legend(loc='upper right')
+	# ax.legend(loc='upper right')
 
 	fig.tight_layout()