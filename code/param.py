9164d0a527e7725fb2e61089b49c38076620cb2a
ben <benjamin.p.riviere@gmail.com>
2020-09-14 22:23:05 -0700

    night run param

--- code/param.py
+++ code/param.py
@@ -67,9 +67,9 @@ class Param:
 		self.device = 'cpu'
 		self.l_mode = "IL" # IL, DAgger, ExIt, Mice # so far only IL is implemented 
 		self.l_parallel_on = True # set to false only for debug 
-		self.l_num_iterations = 2
-		self.l_num_file_per_iteration = 2 # optimized for num cpu on ben's laptop 
-		self.l_num_points_per_file = 25
+		self.l_num_iterations = 10
+		self.l_num_file_per_iteration = 20 # optimized for num cpu on ben's laptop 
+		self.l_num_points_per_file = 800
 		self.l_training_teams = ["a","b"]
 		self.l_robot_team_composition_cases = [
 			{
@@ -112,8 +112,8 @@ class Param:
 		self.l_network_activation = "relu"
 		self.l_test_train_ratio = 0.8
 		self.l_max_dataset_size = 1000000 # n_points 
-		self.l_batch_size = 20
-		self.l_n_epoch = 10
+		self.l_batch_size = 2000
+		self.l_n_epoch = 1000
 		self.l_lr = 1e-3
 		self.l_wd = 0 
 		self.l_log_interval = 1
