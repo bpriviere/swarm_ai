5d1184b749e0329e291a61c5fbf3a41af5f0b328
Wolfgang Hoenig <whoenig@caltech.edu>
2020-10-17 12:47:23 -0700

    bugfix for beta3>0

--- code/cpp/Game.hpp
+++ code/cpp/Game.hpp
@@ -321,7 +321,7 @@ class Game {
     std::uniform_real_distribution<float> dist(0.0,1.0);
     if (!terminal
         && dist(m_generator) < beta3) {
-      float reward =  estimateValue(state, policyAttacker, policyDefender);
+      float reward =  estimateValue2(state, policyAttacker, policyDefender);
       if (!isnan(reward)) {
         return Reward(reward, 1 - reward);
       }
@@ -459,6 +459,40 @@ class Game {
     // // Tie otherwise (essentially timeout)
     // return 0.5;
   }
+
+
+  float estimateValue2(
+    const GameStateT& state,
+    const PolicyT& policyAttacker,
+    const PolicyT& policyDefender)
+  {
+    if (state.turn == GameStateT::Turn::Defenders)
+    {
+      if (policyAttacker.glasConst().valid()) {
+        // we check the turn on the child node, so in this case compute the reward
+        // from the perspective of attackers
+        float value_sum = 0;
+        for (size_t j = 0; j < state.attackers.size(); ++j) {
+          auto result_a = policyAttacker.glasConst().eval(state, m_goal, m_attackerTypes[j], true, j, true);
+          float value = std::get<0>(result_a);
+          value_sum += value;
+        }
+        return value_sum / state.attackers.size();
+      }
+    } else {
+      if (policyDefender.glasConst().valid()) {
+        float value_sum = 0;
+        for (size_t j = 0; j < state.defenders.size(); ++j) {
+          auto result_b = policyDefender.glasConst().eval(state, m_goal, m_defenderTypes[j], false, j, true);
+          float value = std::get<0>(result_b);
+          value_sum += value;
+        }
+        return value_sum / state.defenders.size();
+      }
+    }
+    return std::nanf("");
+  }
+
 
   float estimateValue(
     const GameStateT& state,
