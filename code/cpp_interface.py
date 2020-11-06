fbebcd6cbfe1f098662a673e3643a281b452a422
ben <benjamin.p.riviere@gmail.com>
2020-11-05 19:11:05 -0800

    exp6 working

--- code/cpp_interface.py
+++ code/cpp_interface.py
@@ -296,7 +296,7 @@ def play_game(param,policy_dict_a,policy_dict_b):
 			mctssettings.pw_alpha = policy_dict["mcts_pw_alpha"]
 			mctssettings.beta1 = policy_dict["mcts_beta1"]
 			mctssettings.beta3 = policy_dict["mcts_beta3"]
-			mctssettings.export_tree = (count // 2) % param.tree_timestep == 0  and param.plot_tree_on and count % 2 == 0
+			mctssettings.export_tree = (count // 2) % param.tree_timestep == 0  and param.plot_tree_on 
 
 		if policy_dict["sim_mode"] == "MCTS":
 			depth = gs.depth
@@ -314,8 +314,9 @@ def play_game(param,policy_dict_a,policy_dict_b):
 					sim_result['trees'].append(mctsresult.tree)
 					sim_result['tree_params'].append({
 							'tree_team_1_idxs' : param.team_1_idxs,
+							'tree_team_2_idxs' : param.team_2_idxs,
 							'time' : param.sim_dt*len(sim_result['states']),
-							'robot_idx' : 'Centralized',
+							'robot_idx' : 0,
 						})
 			else:
 				success = False
@@ -346,6 +347,7 @@ def play_game(param,policy_dict_a,policy_dict_b):
 						sim_result['trees'].append(mctsresult.tree)
 						sim_result['tree_params'].append({
 							'tree_team_1_idxs' : team_1_idxs_i,
+							'tree_team_2_idxs' : team_2_idxs_i,
 							'time' : param.sim_dt*len(sim_result['states']),
 							'robot_idx' : robot_idx,
 							})
